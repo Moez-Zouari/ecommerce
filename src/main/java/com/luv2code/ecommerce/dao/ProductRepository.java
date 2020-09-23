308f59e3d7d27c4d21765f7d12ce22c463df297b
Moez-Zouari <moez_zouari@hotmail.fr>
2020-09-23 13:48:04 +0100

    Fixing Bibliotheque pagination Error

--- src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -5,8 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.data.domain.Pageable;
 
-import java.awt.print.Pageable;
+// Il faut faire attention lors de l'importation de la bibliotheque du pagination
+//import java.awt.print.Pageable;
+
+
+
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
