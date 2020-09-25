a422882837a1e15a31199dc0b349e3d0f686af8f
Moez-Zouari <moez_zouari@hotmail.fr>
2020-09-25 16:53:38 +0100

    Modify Spring Boot App - Add a new search method

--- src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -2,10 +2,11 @@ package com.luv2code.ecommerce.dao;
 
 import com.luv2code.ecommerce.entity.Product;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.data.domain.Pageable;
+
 
 // Il faut faire attention lors de l'importation de la bibliotheque du pagination
 //import java.awt.print.Pageable;
@@ -17,4 +18,5 @@ import org.springframework.data.domain.Pageable;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+    Page<Product> findByNameContaining (@RequestParam("name") String name , Pageable pageable);
 }
