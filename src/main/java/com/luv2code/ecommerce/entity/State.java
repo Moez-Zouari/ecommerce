2916d31d4155d0b4647a0c80eb8b911c0b8a6caf
Zouari <moez_zouari@hotmail.fr>
2020-10-12 16:09:38 +0100

    Develop JPA Entities - Country & State

---
+++ src/main/java/com/luv2code/ecommerce/entity/State.java
@@ -0,0 +1,31 @@
+package com.luv2code.ecommerce.entity;
+
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "state")
+@Getter
+@Setter
+@Data
+    public class State {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private  int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private  Country country;
+
+    }
+
+
